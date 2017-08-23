e29979d6fc34ee47275387597f162ca46d78767d
Gina Iliff <giliff@me.com>
2017-08-22 19:36:24 -0700

    Added in access to photo library, and swipe to delet function

---
+++ Collector/ItemsTableViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ItemsTableViewController.swift
+//  Collector
+//
+//  Created by gina iliff on 8/22/17.
+//  Copyright © 2017 gina iliff. All rights reserved.
+//
+
+import UIKit
+
+class ItemsTableViewController: UITableViewController {
+    
+    var items : [Item] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getItems()
+    }
+    
+    func getItems() {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            if let coreDataStuff = try? context.fetch(Item.fetchRequest()) as? [Item] {
+                if let coreDataItems = coreDataStuff {
+                    items = coreDataItems
+                    tableView.reloadData()
+                }
+            }
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let item = items[indexPath.row]
+
+        cell.textLabel?.text = item.title
+        
+        if let imageData = item.image {
+            cell.imageView?.image = UIImage(data: imageData)
+        }
+
+        return cell
+    }
+
+
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        
+        return true
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let item = items[indexPath.row]
+                
+                context.delete(item)
+                try? context.save()
+                
+                getItems()
+            }
+        }
+    }
+}
